31f226cef2ef0a18cadf6039f1740ca1b1810182
Ekin Küçük <ekinkucuk@MacBook-Pro-4.local>
2022-09-10 15:48:48 +0300

    initial commit

---
+++ src/test/java/com/pipelineproject/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipelineproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
